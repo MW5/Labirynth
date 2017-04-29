594f0493fa22b3c3bd1c76bb63372c4d9abd41d7
MW5 <mwojcik1988@gmail.com>
2017-04-29 20:22:33 +0200

    I hate labirynths

--- labirynth/View/ContentController.java
+++ labirynth/View/ContentController.java
@@ -26,13 +26,17 @@ public class ContentController implements Initializable {
     private Labirynth labirynth;
     private ArrayList<WallBlock> walls;
     private ArrayList<Walkway> walkways;
+    public int[][] maze;
     private Runaway runaway;
     public AnimationTimer loop;
     
-    private double colDist = 0;
-    private double interDist = 0;
-    private final double minColDist = 10; //down
-    private final double tryInterval = 80;
+    private boolean startBlockChosen = false;
+    
+    
+    private int x; //startpoints
+    private int y;
+    static char dirNow = 'N';
+    int potato=0;
     
     public void init() {
         labirynth = new Labirynth();
@@ -42,6 +46,7 @@ public class ContentController implements Initializable {
         labirynth.prepareLabirynth();
         walls = labirynth.getWalls();
         walkways = labirynth.getWalkways();
+        this.maze = labirynth.getMaze();
         labirynth.draw(gc);
     }
     
@@ -50,27 +55,33 @@ public class ContentController implements Initializable {
     private void handleMouse() {
         canvas.setOnMouseMoved(new EventHandler<MouseEvent>() {
             public void handle (MouseEvent me) {
-                //System.out.println(me.getTarget());
-                for (Walkway walkway : walkways) {
-                    if (walkway.contains(me.getX(), me.getY())) {
-                        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-                        labirynth.draw(gc);
-                        gc.setStroke(Color.RED);
-                        gc.strokeRect(walkway.getX(), walkway.getY(), walkway.getWidth(), walkway.getHeight());
+                if (!startBlockChosen) {
+                    for (Walkway walkway : walkways) {
+                        if (walkway.contains(me.getX(), me.getY())) {
+                            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                            labirynth.draw(gc);
+                            gc.setStroke(Color.RED);
+                            gc.strokeRect(walkway.getX(), walkway.getY(), walkway.getWidth(), walkway.getHeight());
+                        }
                     }
-                }
-                for (WallBlock wall : walls) {
-                    if (wall.contains(me.getX(), me.getY())) {
-                        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-                        labirynth.draw(gc);
+                    for (WallBlock wall : walls) {
+                        if (wall.contains(me.getX(), me.getY())) {
+                            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                            labirynth.draw(gc);
+                        }
                     }
                 }
             }
         });
+
+        
         canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle (MouseEvent me) {
                 for (Walkway walkway : walkways) {
                     if (walkway.contains(me.getX(), me.getY())) {
+                        y = (int)walkway.getX()/40;
+                        x = (int)walkway.getY()/40;
+                        startBlockChosen = true;
                         runaway = new Runaway();
                         runaway.setCanvas(canvas);
                         runaway.setGraphicsContext(gc);
@@ -94,55 +105,72 @@ public class ContentController implements Initializable {
         });
     }
     
-    private void goLeft(double speed) {
-        runaway.goingUp = false;
-        runaway.goingRight = false;
-        runaway.goingDown = false;
-        runaway.goingLeft = true;
-        double currX = runaway.getX();
-        runaway.setX(currX-=speed);
-        colDist+=1;
-        interDist+=1;
-        System.out.println("Col dist:" + colDist);
-        System.out.println("Inter dist:" + interDist);
-    }
-    private void goUp(double speed) {
-        double currY = runaway.getY();
-        runaway.setY(currY-=speed);
-        runaway.goingUp = true;
-        runaway.goingRight = false;
-        runaway.goingDown = false;
-        runaway.goingLeft = false;
-        colDist+=1;
-        interDist+=1;
-        System.out.println("Col dist:" + colDist);
-        System.out.println("Inter dist:" + interDist);
-
-    }
-    private void goRight(double speed) {
-        double currX = runaway.getX();
-        runaway.setX(currX+=speed);
-        runaway.goingUp = false;
-        runaway.goingRight = true;
-        runaway.goingDown = false;
-        runaway.goingLeft = false;
-        colDist+=1;
-        interDist+=1;
-        System.out.println("Col dist:" + colDist);
-        System.out.println("Inter dist:" + interDist);
-    }
-    private void goDown(double speed) {
-        double currY = runaway.getY();
-        runaway.setY(currY+=speed);
-        runaway.goingUp = false;
-        runaway.goingRight = false;
-        runaway.goingDown = true;
-        runaway.goingLeft = false;
-        colDist+=1;
-        interDist+=1;
-        System.out.println("Col dist:" + colDist);
-        System.out.println("Inter dist:" + interDist);
-    }
+    void walk(){
+        if (dirNow == 'N'){
+                if(maze[x-1][y]==0){
+                        dirNow = 'W';
+                        x--;
+                }else if(maze[x][y-1]==0){
+                        dirNow = 'N';
+                        y--;
+                }else if(maze[x+1][y]==0){
+                        dirNow = 'E';
+                        x++;
+                }else if(maze[x][y+1]==0){
+                        dirNow = 'S';
+                        y++;
+                }
+        }
+        //////////////////////////////
+        else if (dirNow == 'E'){
+                if(maze[x][y-1]==0){
+                        dirNow = 'N';
+                        y--;
+                }else if(maze[x+1][y]==0){
+                        dirNow = 'E';
+                        x++;
+                }else if(maze[x][y+1]==0){
+                        dirNow = 'S';
+                        y++;
+                }else if(maze[x-1][y]==0){
+                        dirNow = 'W';
+                        x--;
+                }
+        }
+        ///////////////////////////////////
+        else if (dirNow == 'S'){
+                if(maze[x+1][y]==0){
+                        dirNow = 'E';
+                        x++;
+                }else if(maze[x][y+1]==0){
+                        dirNow = 'S';
+                        y++;
+                }else if(maze[x-1][y]==0){
+                        dirNow = 'W';
+                        x--;
+                }else if(maze[x][y-1]==0){
+                        dirNow = 'N';
+                        y--;
+                }
+        }
+        ///////////////////////////
+        else if (dirNow == 'W'){
+                if(maze[x][y+1]==0){
+                        dirNow = 'S';
+                        y++;
+                }else if(maze[x-1][y]==0){
+                        dirNow = 'W';
+                        x--;
+                }else if(maze[x][y-1]==0){
+                        dirNow = 'N';
+                        y--;
+                }else if(maze[x+1][y]==0){
+                        dirNow = 'E';
+                        x++;
+                }
+            }
+            gc.fillRect(y*40, x*40, 40, 40);
+        }
     
     private void startAnimation() {
         loop = new AnimationTimer() {
@@ -153,108 +181,15 @@ public class ContentController implements Initializable {
             @Override
             public void handle(long now) {
                 
-                    for (WallBlock wall : walls) {
-                        if (runaway.intersects(wall.getBoundsInLocal()) && runaway.goingUp) {
-//                            System.out.println(colDist);
-                            double currY = runaway.getY();
-                            runaway.setY(currY+5);
-                            System.out.println("Coll UP");
-                            if (colDist==minColDist) {
-                                colDist = 0;
-                                interDist = 0;
-                                goRight(speed);
-                            } else {
-                                colDist = 0;
-                                interDist = 0;
-                                goLeft(speed);
-                            }
-                        }
-                        if (runaway.intersects(wall.getBoundsInLocal()) && runaway.goingRight) {
-//                            System.out.println(colDist);
-                            double currX = runaway.getX();
-                            runaway.setX(currX-5);
-                            System.out.println("Coll RIGHT");
-                            if (colDist==minColDist) {
-                                colDist = 0;
-                                interDist = 0;
-                                goDown(speed);
-                            } else {
-                                colDist = 0;
-                                interDist = 0;
-                                goUp(speed);
-                            }
-                        }
-                        if (runaway.intersects(wall.getBoundsInLocal()) && runaway.goingDown) {
-//                            System.out.println(colDist);
-                            double currY = runaway.getY();
-                            runaway.setY(currY-5);
-                            System.out.println("Coll DOWN");
-                            if (colDist==minColDist) {
-                                colDist = 0;
-                                interDist = 0;
-                                goLeft(speed);
-                            } else {
-                                colDist = 0;
-                                interDist = 0;
-                                goRight(speed);
-                            }
-                        }
-                        if (runaway.intersects(wall.getBoundsInLocal()) && runaway.goingLeft) {
-                            double currX = runaway.getX();
-                            runaway.setX(currX+5);
-                            System.out.println("Coll LEFT");
-                            if (colDist==minColDist) {
-                                colDist = 0 ;
-                                interDist = 0;
-                                goUp(speed);
-//                                System.out.println("UP");
-                            } else {
-                                colDist = 0;
-                                interDist = 0;
-                                goDown(speed);
-//                                System.out.println("DOWN"+dist);
-                            }
-                        }
-                    }
-                    
-                    if (runaway.goingLeft) {
-//                        if(interDist == tryInterval) {
-//                            System.out.println("try down");
-//                            interDist = 0;
-//                            goDown(speed);
-//                        } else {
-                            goLeft(speed);
-//                        }
-                    }
-                    if (runaway.goingUp) {
-//                        System.out.println(dist);
-//                        if(interDist == tryInterval) {
-//                            interDist = 0;
-//                            goLeft(speed);
-//                        } else {
-                            goUp(speed);
-//                        }
-                    }
-                    if (runaway.goingRight) {
-//                        System.out.println(dist);
-//                        if(interDist == tryInterval) {
-//                            interDist = 0;
-//                            goUp(speed);
-//                        } else {
-                            goRight(speed);
-//                        }
-                    }
-                    if (runaway.goingDown) {
-//                        System.out.println(dist);
-//                        if(interDist == tryInterval) {
-//                            interDist = 0;
-//                            goRight(speed);
-//                        } else {
-                            goDown(speed);
-//                        }
-                    }
                 
-                runaway.draw();
+                while(!(x==1 && y==1 )){
+                        walk();	
+                        potato++;
+                        //runaway.draw();
+                }
+                
+                
+//                runaway.draw();
 
                 if( runaway.getX() <= endX) {
                     loop.stop();
